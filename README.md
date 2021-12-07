c65a8083e206a91aeb10fda9364494def3dc06f4
Kristof Poszet <kristof_poszet@epam.com>
2021-12-07 18:19:50 +0100

    Initial folder structure and configurations

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # front-team
+
+To start:
+cd front-team
+npm start
+
+For linting:
+npm run lint
+OR
+npm run lint:fix
---
+++ front-team/src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/HelloWorld.css';
+
+const HelloWorld = () => (
+  <h2 className="HelloWorld">Hello world!</h2>
+);
+
+export default HelloWorld;
