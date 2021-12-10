d64b78be15e1a766f4f233226db68ad4fd5c1454
Vika28 <v.nakoryk-2022@kpi.ua>
2021-12-10 17:04:30 +0200

    add some styles to header, create folder for buttons with lang.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar} from "react-bootstrap";
 import { useIntl } from 'react-intl';
-import BtnsLanguages from "../btnsLanguages";
+import BtnsLanguages from "../BtnsLanguages/btnsLanguages";
 import PropTypes from "prop-types";
 
 const messages = {
@@ -22,7 +22,7 @@ const Header = (props) => {
         <Navbar className={`m-0 ${styles.navbarWrapper}`}>
             <div className={`d-flex justify-content-between ${styles.navbarContainer}`}>
                 <Navbar.Brand as={Link} to="/" className='d-block'>Recycling</Navbar.Brand>
-                <Nav className="me-auto">
+                <Nav className={`${styles.pagesWrapper} me-auto`}>
                     <Nav.Link as={Link} to="/">{intl.formatMessage({ id: messages.homeMsg })}</Nav.Link>
                     <Nav.Link as={Link} to="/map">{intl.formatMessage({ id: messages.mapMsg })}</Nav.Link>
                     <Nav.Link as={Link} to="/articles">{intl.formatMessage({ id: messages.articlesMsg })}</Nav.Link>
