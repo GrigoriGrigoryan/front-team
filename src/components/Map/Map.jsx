47e7856bb1ae0e0984f5061e808a0e59916276a4
Kristof Poszet <kristof_poszet@epam.com>
2021-12-10 12:51:18 +0100

    responsive map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,19 +10,26 @@ import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 const Map = () => {
   return (
-    <div className='Map'>
-      <MapContainer className='Map' center={[59.9375, 30.308611]} zoom={13} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
-          url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
-          maxZoom={16}
-          minZoom={3}
-          noWrap={true}
-        />
-        <MarkerClusterGroup>
-          <MapMarkers />
-        </MarkerClusterGroup>
-      </MapContainer>
+    <div className='MapContainer'>
+      <div className='Map'>
+        <MapContainer
+          className='Map'
+          center={[59.9375, 30.308611]}
+          zoom={13}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+            maxZoom={16}
+            minZoom={3}
+            noWrap={true}
+          />
+          <MarkerClusterGroup>
+            <MapMarkers />
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
       <div className='GarbageTypes'>
         <h2>Different icons according to the waste types will belong here..</h2>
       </div>
