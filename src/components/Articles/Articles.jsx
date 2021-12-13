c23d84559b1a5d3676810cb18881ac111b91b317
Vika28 <v.nakoryk-2022@kpi.ua>
2021-12-13 17:53:36 +0200

    add underline on active link in header. fix bug on page with articles and footer

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Articles.module.css';
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
+import NavLink from "react-router-dom/es/NavLink";
 
 const Articles = () => {
     const articlesArr = [
@@ -27,6 +28,24 @@ const Articles = () => {
             title: 'What Would Happen If People Stopped Recycling?',
             img: 'https://media-exp1.licdn.com/dms/image/C4D1BAQEnbNMKOBsoAQ/company-background_10000/0/1538146833032?e=2159024400&v=beta&t=N2APXODb9TgWwUTXiLXGC-XPopqh72HAJ6NE4Xm2JwE',
             text: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'
+        },
+        {
+            id: 5,
+            title: 'What Would Happen If People Stopped Recycling?',
+            img: 'https://media-exp1.licdn.com/dms/image/C4D1BAQEnbNMKOBsoAQ/company-background_10000/0/1538146833032?e=2159024400&v=beta&t=N2APXODb9TgWwUTXiLXGC-XPopqh72HAJ6NE4Xm2JwE',
+            text: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'
+        },
+        {
+            id: 6,
+            title: 'What Would Happen If People Stopped Recycling?',
+            img: 'https://media-exp1.licdn.com/dms/image/C4D1BAQEnbNMKOBsoAQ/company-background_10000/0/1538146833032?e=2159024400&v=beta&t=N2APXODb9TgWwUTXiLXGC-XPopqh72HAJ6NE4Xm2JwE',
+            text: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'
+        },
+        {
+            id: 7,
+            title: 'What Would Happen If People Stopped Recycling?',
+            img: 'https://media-exp1.licdn.com/dms/image/C4D1BAQEnbNMKOBsoAQ/company-background_10000/0/1538146833032?e=2159024400&v=beta&t=N2APXODb9TgWwUTXiLXGC-XPopqh72HAJ6NE4Xm2JwE',
+            text: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'
         }
     ]
     return (
@@ -36,11 +55,10 @@ const Articles = () => {
                     return (
                         // eslint-disable-next-line react/jsx-key
                         <div className={styles.articleItem}>
-                            <Link to={`/article/${item.id}`}>
+                            <NavLink to={`/article/${item.id}`}>
                                 <h4 className={styles.articleTitle}>{item.title}</h4>
                                 <img className={styles.articleImg} src={item.img} alt=""/>
-                                {/*<p className={styles.articleText}>{item.text}</p>*/}
-                            </Link>
+                            </NavLink>
                         </div>
                     )
                 })
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar } from 'react-bootstrap';
@@ -16,6 +16,12 @@ const messages = {
 };
 const Header = (props) => {
   const intl = useIntl();
+  const checkActive = (match, location) => {
+    if(!location) return false;
+    const {pathname} = location;
+    return pathname === "/";
+  }
+
   return (
     <Navbar className={`m-0 ${styles.navbarWrapper}`} expand='md'>
       <div className={`d-flex justify-content-between container ${styles.navbarContainer}`}>
@@ -25,20 +31,18 @@ const Header = (props) => {
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav' className={styles.navbarCollapse}>
           <Nav className={`${styles.pagesWrapper}`}>
-            <Nav.Link as={Link} to='/' activeClassName={styles.active}>
-              <span className={styles.navLink}>
+            <NavLink to='/' className={styles.linkWrapper} activeClassName={styles.active} isActive={checkActive}>
                 {intl.formatMessage({ id: messages.homeMsg })}
-              </span>
-            </Nav.Link>
-            <Nav.Link as={Link} to='/map' className={styles.linkWrapper}>
-              <span className={styles.navLink}>{intl.formatMessage({ id: messages.mapMsg })}</span>
-            </Nav.Link>
-            <Nav.Link as={Link} to='/articles' className={styles.linkWrapper}>
-              <span className={styles.navLink}>{intl.formatMessage({ id: messages.articlesMsg })}</span>
-            </Nav.Link>
-            <Nav.Link as={Link} to='/garbageLibrary' className={styles.linkWrapper}>
-              <span className={styles.navLink}>{intl.formatMessage({ id: messages.garbageLibraryMsg })}</span>
-            </Nav.Link>
+            </NavLink>
+            <NavLink to='/map' className={styles.linkWrapper} activeClassName={styles.active}>
+                {intl.formatMessage({ id: messages.mapMsg })}
+            </NavLink>
+            <NavLink to='/articles' className={styles.linkWrapper} activeClassName={styles.active}>
+                {intl.formatMessage({ id: messages.articlesMsg })}
+            </NavLink>
+            <NavLink to='/garbageLibrary' className={styles.linkWrapper} activeClassName={styles.active}>
+                {intl.formatMessage({ id: messages.garbageLibraryMsg })}
+            </NavLink>
           </Nav>
           <BtnsLanguages localeHandler={props.localeHandler} />
         </Navbar.Collapse>
