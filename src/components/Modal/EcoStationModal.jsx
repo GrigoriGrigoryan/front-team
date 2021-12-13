76e19a9aab41bb25ac3b4c1d5019bb80320e69ec
Kristof Poszet <kristof_poszet@epam.com>
2021-12-13 13:55:20 +0100

    modal - title (with rating) and image added

--- src/components/Modal/EcoStationModal.jsx
+++ src/components/Modal/EcoStationModal.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, Image } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import '../styles/Button.css';
+import './EcoStationModal.css';
+import '../styles/CustomPopup.css';
 
 const EcoStationModal = ({ ecoStationName, rating, wasteTypes, deliveryOptions, paymentCondition, ...props }) => {
   return (
     <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
       <Modal.Header closeButton>
-        <Modal.Title id='contained-modal-title-vcenter'>{ecoStationName}</Modal.Title>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          <div className='PopupHeader'>
+            <div className={`Rating ${rating > 4 ? 'green' : props.rating > 2.5 ? 'orange' : 'red'}`}>
+              {(Math.round(rating * 100) / 100).toFixed(1)}
+            </div>
+            <h4 className='EcoStationTitle ModalTitle'>{ecoStationName}</h4>
+          </div>
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>{ecoStationName}</h4>
-        <p>
-          {rating} {wasteTypes[0]} {deliveryOptions[0]} {paymentCondition.price}
-        </p>
+        <div className='ModalContainer'>
+          <Image
+            className='EcoImg'
+            src='https://www.leduc.ca/sites/default/files/EcoStation_sign-image.png'
+            alt='eco station'
+            fluid
+          />
+          <p>
+            {wasteTypes[0]} {deliveryOptions[0]} {paymentCondition.price}
+          </p>
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <button className='butt' onClick={props.onHide}>
